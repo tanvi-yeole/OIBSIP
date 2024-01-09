c9fee04746caf44cb34ba914c63be3330eacbf5d
tanvi-yeole <111522405+tanvi-yeole@users.noreply.github.com>
2024-01-09 16:51:07 +0530

    Div management and improved css

--- Task1/script.js
+++ Task1/script.js
@@ -10,6 +10,10 @@ Array.from(buttons).forEach((button)=>{
       string = ""
       document.querySelector('input').value = string;
     }
+    else if(e.target.innerHTML == '+/-'){
+      string = "(-"
+      document.querySelector('input').value = string;
+    }
     else{ 
     console.log(e.target)
     string = string + e.target.innerHTML;
